0be0a294865f7446df7d4fcb953ee8386dddd842
Sviatoslav-Mulka <144914870+Sviatoslav-Mulka@users.noreply.github.com>
2024-03-15 09:17:06 +0200

    Add files via upload

---
+++ 1Mod/1Mod/Program.cs
@@ -0,0 +1,39 @@
+﻿//v2
+using System;
+
+public enum DaysOfWeek
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+public class CalendarEvent
+{
+    public string EventName { get; set; }
+    public DaysOfWeek EventDay { get; set; }
+
+    public CalendarEvent(string eventName, DaysOfWeek eventDay)
+    {
+        EventName = eventName;
+        EventDay = eventDay;
+    }
+
+    public void PrintEventDetails()
+    {
+        Console.WriteLine($"Подія {EventName} відбудеться в день {EventDay}.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CalendarEvent myEvent = new CalendarEvent("Концерт", DaysOfWeek.Saturday);
+        myEvent.PrintEventDetails();
+    }
+}
